89f91b228532c9bcf4f6cdc45be18cd03949bdf4
Kazi-Yeasin <kaziyeasinarafat221@gmail.com>
2025-10-26 22:15:14 +0600

    linked list

---
+++ linked_list/LinkedList.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    node struct {
+        int data;
+        node struct* next;
+    }
+    node* N(){
+        *next = (struct*)malloc(sizeof(node struct));
+        return *next;
+    }
+    return 0;
+}
