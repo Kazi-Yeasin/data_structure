6c89e16853d3659b2b79b43d7581f6860fa4f392
Kazi-Yeasin <kaziyeasinarafat221@gmail.com>
2025-11-13 21:53:20 +0600

    linked list creation

--- linked_list/LinkedList.c
+++ linked_list/LinkedList.c
@@ -1,5 +1,37 @@
 #include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next; 
+};
+
 int main (){
-   cout<<"linked list";
+    printf("Enter the size of List : \n");
+    int n; scanf("%d", &n);
+    printf("Enter the data : \n");
+
+    struct node *head = NULL;
+    struct node *tail = NULL;
+
+    while(n--){
+        int x; scanf("%d", &x);
+        struct node *newcode = (struct node*)malloc(sizeof(int *));
+        newcode->data = x;
+        newcode->next = NULL;
+       
+        if(head == NULL){
+            head=tail=newcode;
+        }else{
+            tail->next = newcode;
+            tail = tail->next;
+        }
+    }
+    struct node *ptr = head;
+    while (ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
     return 0;
 }
